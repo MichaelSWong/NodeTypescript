6696e7c7ce0e45e10c14f7cbc53dc47abb3c80dc
MICHAELSWONG <mizik_jah@yahoo.com>
2020-09-01 18:18:04 -0400

    added skeleton and env files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
 const app = express();
-const port = 8080; // default port to listen
+
+// Configure Express to use EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
 // start the Express server
